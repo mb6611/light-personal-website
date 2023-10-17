254e47bf96f0a0195436851e95eab05ab805653d
Matthew Banaag <mb6611@princeton.edu>
2023-10-16 22:06:46 -0400

    added header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Header.css'
+import MatthewHeadshot from '../../assets/Matthew Headshot large.png'
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="header">
+            <img className="matthew-headshot" alt="Matthew headshot" src={MatthewHeadshot}/>
+            <p>
+                Hey, I’m Matthew. <span className="emoji">👋</span><br/><br/>
+                I build AI tech and startups.<span className="emoji">⚡</span>
+            </p>
         </div>
     )
 }
